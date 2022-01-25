8877370ced6c6a3a8e1591da6429f341a587901a
N00bG1rl <marttila.helen@gmail.com>
2022-01-25 11:29:14 +0200

    localStorage added

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,24 +10,30 @@ const AuthContext = React.createContext({
 
 // Export as name export
 export const AuthContextProvider = props => {
-  const [token, setToken] = useState(null)
+  // Get token from localStorage
+  const initialToken = localStorage.getItem('token')
+  // null or token
+  const [token, setToken] = useState(initialToken)
 
   // Converts true or false
   const userIsLoggedIn = !!token
 
   const loginHandler = token => {
     setToken(token)
+    // Store in localStorage
+    localStorage.setItem('token', token)
   }
 
   const logoutHandler = () => {
     setToken(null)
+    localStorage.removeItem('token')
   }
 
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
     login: loginHandler,
-    logout: loginHandler,
+    logout: logoutHandler,
   }
 
   return (
